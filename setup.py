5deae7113e8927ad54b2bde9baa60281bfcb6f9f
Nikolay <billionspoint@gmail.com>
2019-02-16 12:15:31 +0300

    init

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name='Filish Storage',
+    license='MIT',
+    author='Nikolay Oceean',
+    packages=['botinsec'],
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        # Nothing yet
+    ],
+    # entry_points={
+    #     'console_scripts': [
+    #         'filish = filish:cli',
+    #     ],
+    # },
+)
